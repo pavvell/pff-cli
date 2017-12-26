7a04c43f7924cb89a1ff64896b40a16ae046c354
Pavel Nesmelov <pavel.nesmelov@piano.io>
2017-12-26 18:35:51 +0400

    update

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ let git = require('simple-git')();
 let commander = require('commander');
 let exec = require('child_process').exec;
 let CLI = require('clui');
-//let Spinner = CLI.Spinner;
+let Spinner = CLI.Spinner;
 
-function Spinner() {
-  this.start = function () {}
-  this.stop = function () {}
-}
 
 let actionList = [
   {name: 'Publish updates (patch)', value: 'publishPatch'}
@@ -28,13 +24,12 @@ async function publishPatch() {
 }
 
 function patchNpmVersion() {
-  let status = new Spinner('Update version...');
-  status.start();
+  console.log('Update version...');
 
   return new Promise((resolve, reject) => {
     exec("npm version patch", function (error, stdout, stderr) {
-      status.stop();
-
+      console.log(stdout);
+      
       if (error !== null) {
         console.log('exec error: ' + error);
         reject(error);
@@ -46,12 +41,10 @@ function patchNpmVersion() {
 }
 
 function publish() {
-  let status = new Spinner('Publish...');
-  status.start();
+  console.log('Publish...');
 
   return new Promise((resolve, reject) => {
     exec("npm publish", function (error, stdout, stderr) {
-      status.stop();
 
       if (error !== null) {
         console.log('exec error: ' + error);
@@ -64,9 +57,6 @@ function publish() {
 }
 
 function justPush() {
-  let status = new Spinner('Push...');
-  status.start();
-
   return new Promise(resolve => {
     git
       .push('origin', 'master')
@@ -96,12 +86,7 @@ function askCommitMessageAndCommit() {
     ];
 
     inquirer.prompt(questions).then(answers => {
-      let status = new Spinner('Committing...');
       let commitMessage = answers.commitMessage;
-
-      status.start();
-
-      console.log('answers', answers);
 
       git
         .add('./*')
@@ -133,7 +118,6 @@ function askAction() {
 }
 
 function execAction(actionName) {
-  console.log('run ', actionName);
   actions[actionName]();
 }
 
